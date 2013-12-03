ca8ebfcc790aafbc84372ab4c0e3dab45de18e15
Dražen Lučanin <kermit666@gmail.com>
2013-12-03 23:35:08 +0100

    yo gruntplugin

---
+++ tasks/django.js
@@ -0,0 +1,51 @@
+/*
+ * grunt-django
+ * https://github.com/kermit666/grunt-django
+ *
+ * Copyright (c) 2013 Dražen Lučanin
+ * Licensed under the GPLv3 license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('django', "Grunt task for wrapping references with Django's url template tags (for integration with Django's static assets and easy CDN switching).", function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
